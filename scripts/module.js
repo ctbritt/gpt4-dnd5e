dc01ac291b1dbafe4f4fbcee3feb6b4ebc8fc358
Chris Allbritton <callbritton@mac.com>
2023-03-15 15:52:16 -0400

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,56 @@
-Hooks.once('init', async function() {
+Hooks.once('init', () => {
+  console.log('GPT-4 D&D Rules | Initializing GPT-4 D&D Rules module');
 
+  game.settings.register('gpt4-dnd5e', 'apiKey', {
+	name: 'ChatGPT API Key',
+	hint: 'Enter your ChatGPT API key from OpenAI.',
+	scope: 'world', // The API key setting will be available only to the GM
+	config: true,
+	default: '',
+	type: String
+  });
 });
+async function callGPT4Api( prompt ) {
+	const GPT4_API_KEY = game.settings.get('gpt4-dnd5e', 'apiKey');
 
-Hooks.once('ready', async function() {
+	const response = await fetch( 'https://api.openai.com/v1/chat/completions', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${GPT4_API_KEY}`
+		},
+		body: JSON.stringify( {
+			model: "gpt-3.5-turbo",
+			messages: [
+				{ 'role': 'user', 'content': prompt }
+			],
+			max_tokens: 200,
+			n: 1,
+			stop: null,
+			temperature: 0.5
+		} )
+	} );
 
-});
+	const data = await response.json( );
+	return data.choices[ 0 ].message.content.trim( );
+}
+
+Hooks.on( 'chatMessage', async ( chatLog, message, chatData ) => {
+	// Check if the message starts with a specific command, like "!gpt4"
+	if ( message.startsWith( '?' ) ) {
+		const question = message.slice( 1 ).trim( );
+		console.log( "question:", question );
+
+		const prompt = `You are a AI Dungeon Master for Dungeons and Dragons 5e. You can answer any questions regarding rules and other lore about D&D. If the question about a monster, format your answer in the form of a D&D 5e statblock. You should stick to the text of the various rulebooks and don't add anything. ${question}`
+		const answer = await callGPT4Api( prompt );
+
+		// Create a new chat message with the answer
+		let chatMessage = await ChatMessage.create( {
+			user: game.user._id,
+			content: answer
+		} );
+
+		// Prevent the original message from being displayed
+		return false;
+	}
+} );
